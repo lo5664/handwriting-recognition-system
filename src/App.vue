8dc2718d58c407fecb2d7563492595244fefd6ad
lo5664 <3343574043@qq.com>
2026-03-30 17:20:01 +0800

    Create App.vue as main component

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+// src/App.vue
+<template>
+  <div id="app">
+    <HeaderComponent />
+    <main>
+      <router-view />
+    </main>
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
+export default {
+  components: {
+    HeaderComponent,
+    FooterComponent,
+  },
+};
+</script>
+
+<style>
+/* Add your styles here */
+</style>
